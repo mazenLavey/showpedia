9a7e34062d94f824e77e336ef7e1ddfecb0eebc2
Mazen Al-Madhage <mazen.fp@gmail.com>
2024-01-14 16:31:23 +0300

    adding show page animation

--- src/pages/showPage/ShowBackground.jsx
+++ src/pages/showPage/ShowBackground.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import GetShowBackground from '../../data/GetShowBackground';
 import { Blurhash  } from "react-blurhash";
 import ShowBackgroundCSS from './css/ShowBackground.module.css';
 
 const ShowBackground = ({data})=>{
-    const [bgImgLoaded, setBgImgLoaded] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [bgImg] = GetShowBackground(data.id);
+    const imgRef = useRef(null);
 
     function handelLoadingBgImg(e){
-        setBgImgLoaded(e.target.complete);
+        setIsLoading(!e.target.complete);
+        imgRef.current.classList.add('fade-in');
     }
 
     return (
         <div className={ShowBackgroundCSS.background}>
-            {bgImgLoaded? null: <Blurhash hash='LGC6rnIV00-p~qV@4otRENt6xCNb' width={"100%"} height={"100%"} className={ShowBackgroundCSS.bg__blurhash} style={{zIndex: 1}}/>}
-            <img src={bgImg} alt={data.name} onLoad={handelLoadingBgImg}/>
+            {isLoading && <Blurhash hash='LGC6rnIV00-p~qV@4otRENt6xCNb' width={"100%"} height={"100%"} className={ShowBackgroundCSS.bg__blurhash} style={{zIndex: 1}}/>}
+            <img ref={imgRef} src={bgImg} alt={data.name} onLoad={handelLoadingBgImg}/>
         </div>
     );
 };
